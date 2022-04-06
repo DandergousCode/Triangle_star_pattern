0c860ce6984f00799902991dc003c79f470e2b92
S.Majumder <94290293+DandergousCode@users.noreply.github.com>
2022-04-06 21:54:01 +0530

    Mirrored right angle triangle.

---
+++ pattern_5.java
@@ -0,0 +1,20 @@
+public class pattern_5 
+{
+    public static void main(String[] args) 
+    {
+        int n = 4;
+
+        for(int i=n; i>0; i--)
+        {
+            for(int j=i; j>0; j--)
+            {
+                System.out.print(" ");
+            }
+            for(int j=n+1; j>i; j--)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }    
+}
